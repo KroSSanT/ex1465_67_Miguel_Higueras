c5a5a416b6ad16e64128c0ef70480f78cbf05cf2
Miguel Higueras <mhiguerasp@gmail.com>
2024-06-27 16:48:28 +0200

    prints_strs v1

---
+++ ej02/prints_strs.c
@@ -0,0 +1,47 @@
+# include <unistd.h>
+
+void *print_strs(int const arg, char const *str)
+{
+    int i;
+
+    i = 0;
+    if (arg%2 == 0)
+    {
+        while (str[i] != '\0')
+        {
+            i++;
+        }
+
+        while (i >= 0)
+        {
+            write (1, &str[i], 1);
+            i--;
+        }
+        write (1, &"\n", 1);
+    }
+    else
+    {
+        while (str[i] != '\0')
+        {
+            write (1, &str[i], 1);
+            i++;
+        }
+        write (1, &"\n",1);
+    }
+}
+
+int main (int argc, char **argv)
+{   
+    int i;
+    int j;
+
+    i=1;
+    j=1;
+    while (argc != i)
+    {
+        print_strs(argc, argv[j]);
+        i++;
+        j++;
+    }
+    return (0);
+}   
