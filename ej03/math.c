45e109e298b6ecd4794376d03fe2a7571c81fc96
Miguel Higueras <mhiguerasp@gmail.com>
2024-06-27 19:15:48 +0200

    math.c con array

--- ej03/math.c
+++ ej03/math.c
@@ -7,10 +7,14 @@ void math(char const *num)
     int x;
     int y;
     int i;
+    int j;
 
+    int result[10];
+    
     x = atoi(num);
     i = 1;
-    
+    j = 0;
+
     while (i <= 10)
     {
         y = x*i;
@@ -19,13 +23,35 @@ void math(char const *num)
             printf ("%d x %d = %d <Par>\n",i,x,y);
         else
             printf ("%d x %d = %d <Impar>\n",i,x,y);
+        
+        result[j] = y;
+        j++;
         i++;
+    }
+
+    printf("\nArray de resultados:\n");
+    j = 0;
+    while (j <1)
+    {
+        printf("%d,",result[j]);
+        j++;
+    } 
+    while ( j <= 8)
+    {
+        printf(" %d,",result[j]);
+        j++;
+    }
+    while (j <= 9)
+    {
+        printf(" %d",result[j]);
+        j++;
     }
         
 }
 
 int main (int argc, char **argv)
 {   
+    
     if (argc != 2)
     {
         write (1, &"Error",5);
